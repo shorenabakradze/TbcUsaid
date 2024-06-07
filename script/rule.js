829b2ec766d9a9d62ea66de207b3b2e80383f97f
shorenabakradze <136688121+shorenabakradze@users.noreply.github.com>
2024-06-07 21:34:37 +0400

    finished

--- script/rule.js
+++ script/rule.js
@@ -5,3 +5,7 @@ document.getElementById('terms').addEventListener('click', function() {
   document.getElementById('closeMenu').addEventListener('click', function() {
     document.getElementById('offScreenMenu').classList.remove('show');
   });
+  
+  document.getElementById('closeMenu1').addEventListener('click', function() {
+    document.getElementById('offScreenMenu').classList.remove('show');
+  });
--- script/slider.js
+++ script/slider.js
@@ -1,29 +1,31 @@
-let slideIndex = 1;
-showSlides(slideIndex);
+let slideIndex = 0;
+  showSlides();
 
-function plusSlides(n) {
+  function plusSlides(n) {
     showSlides(slideIndex += n);
-}
+  }
 
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
+  function currentSlide(n) {
+    showSlides(slideIndex = n - 1);
+  }
 
-function showSlides(n) {
+  function showSlides() {
     let i;
     let slides = document.getElementsByClassName("mySlide");
-    let dots = document.getElementsByClassName("dots");
-    if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
+    let dots = document.getElementsByClassName("dot");
+    if (slideIndex >= slides.length) {slideIndex = 0}
+    if (slideIndex < 0) {slideIndex = slides.length - 1}
     for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
+      slides[i].style.display = "none";
     }
     for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
+      dots[i].className = dots[i].className.replace(" active", "");
     }
-    slides[slideIndex-1].style.display = "block";
-    dots[slideIndex-1].className += " active";
-}
+    slides[slideIndex].style.display = "flex";
+    dots[slideIndex].className += " active";
+    slideIndex++;
+    setTimeout(showSlides, 5000); 
+  }
 
 
 
